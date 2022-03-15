61f6d110a54cecfce628db7bd972e14372152cd8
EElom <ye.essan@gmail.com>
2022-03-15 17:54:23 +0100

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,34 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return "${this.strength}";
+  }
+  receiveDamage(TheDamage) {
+    return "${this.health}";
+  }
+}
 
 // Viking
-class Viking {}
+class Viking {
+  constructor(name, health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return "${this.strength}";
+  }
+  receiveDamage(theDamage) {
+    let alive = true;
+    if ((alive = true)) {
+      return "${this.name} has received DAMAGE points of damage";
+    }
+  }
+}
 
 // Saxon
 class Saxon {}
